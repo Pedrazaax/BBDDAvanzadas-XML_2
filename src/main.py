2e8244ff1e1adfa63121b1f0d115cd54ea81a28d
Ismaqm <Ismael.Quijorna@alu.uclm.es>
2023-04-05 14:25:00 +0200

    avances

--- src/main.py
+++ src/main.py
@@ -1,7 +1,20 @@
 import xml.etree.ElementTree as ET
+import sqlite3
+
+# Crear una conexión a la base de datos SQLite3
+conn = sqlite3.connect('fondos.db')
+c = conn.cursor()
+
+# Crear una tabla para almacenar los datos del fondo
+c.execute('''
+    CREATE TABLE IF NOT EXISTS fondos (
+        registro TEXT PRIMARY KEY,
+        nombre TEXT
+    )
+''')
 
 # Lista con los nombres de los archivos XML
-xml_files = ['fondo1.xml', 'fondo2.xml', 'fondo3.xml', 'fondo4.xml', 'fondo5.xml', 'fondo6.xml', 'fondo7.xml', 'fondo8.xml']
+xml_files = ['semestre1_2019.XML']
 
 # Define un espacio de nombres para los elementos XBRL
 ns = {'xbrl': 'http://www.xbrl.org/2003/instance'}
@@ -11,7 +24,19 @@ for xml_file in xml_files:
     tree = ET.parse(xml_file)
     root = tree.getroot()
     
-    # Busca todos los elementos con la etiqueta "xbrl:elemento" y extrae su valor
-    for elemento in root.findall('.//xbrl:elemento', ns):
-        valor = elemento.text
-        print(valor)
+    elemento = root.find('.//xbrl:identifier', ns)
+    valor = elemento.text
+    print(valor)
+
+    
+
+    # Define un espacio de nombres para los elementos XBRL
+    ns1 = {'iic-com': 'http://www.cnmv.es/iic/com/1-2009/2009-03-31'}
+
+    # Busca el primer elemento con la etiqueta "iic-com:RegistroCNMV" y extrae su valor
+    registro = root.find('.//iic-com:RegistroCNMV', ns1)
+    valorRegistro= registro.text
+    c.execute('INSERT INTO fondos VALUES (?,?)', (valorRegistro,valor,))
+
+conn.commit()
+conn.close()
